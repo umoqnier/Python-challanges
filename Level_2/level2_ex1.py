2a0c142a437b10e85144bf25f972b287cdabb5d3
Oscar Romeo Ramos Molina <RomeoRamos@MacBook-Pro-de-Oscar-2.local>
2017-04-13 23:17:22 -0500

    Level2 Ex 1 done

--- Level_2/level2_ex1.py
+++ Level_2/level2_ex1.py
@@ -1,6 +1,6 @@
 """
-Write a program which accepts a sequence of comma separated 4 digit binary numbers 
-as its input and then check whether they are divisible by 5 or not. 
+Write a program which accepts a sequence of comma separated 4 digit binary numbers
+as its input and then check whether they are divisible by 5 or not.
 The numbers that are divisible by 5 are to be printed in a comma separated sequence.
 Example:
 0100,0011,1010,1001
@@ -12,3 +12,9 @@ Hint:
 use 'list comprehension'
 
 """
+
+numList = input("Enter your list of numbers >>>")
+
+numberList = numList.split(",")
+DivNumbers = [int(x) for x in numberList if int(x) % 5 == 0]
+print(DivNumbers)
