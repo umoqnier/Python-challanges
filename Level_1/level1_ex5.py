abcbbf688c21a1729343e1b45da6c379d1c9d882
Oscar Romeo Ramos Molina <RomeoRamos@MacBook-Pro-de-Oscar-2.local>
2017-04-13 22:19:57 -0500

    Level 1 Ex 5 done

--- Level_1/level1_ex5.py
+++ Level_1/level1_ex5.py
@@ -1,13 +1,30 @@
 """
-Write a program that asks the user how many Fibonnaci numbers to generate and 
-then generates them. 
-Take this opportunity to think about how you can use functions. 
+Write a program that asks the user how many Fibonnaci numbers to generate and
+then generates them.
+Take this opportunity to think about how you can use functions.
 Make sure to ask the user to enter the number of numbers in the sequence to generate.
 
-Hint 
-The Fibonnaci seqence is a sequence of numbers where the next number 
-in the sequence is the sum of the previous two numbers in the sequence. 
-The sequence looks like this: 1, 1, 2, 3, 5, 8, 13, 
+Hint
+The Fibonnaci seqence is a sequence of numbers where the next number
+in the sequence is the sum of the previous two numbers in the sequence.
+The sequence looks like this: 1, 1, 2, 3, 5, 8, 13,
 
 
 """
+def fibonnacci():
+    number = int(input("Introduce a number of Fibonnaci steps:"))
+    lastNumber = 1
+    thisNumber = 1
+    nextNumber = 0
+    listNumber = [lastNumber, thisNumber]
+
+    for q in range(number):
+        nextNumber = lastNumber + thisNumber
+        listNumber.append(nextNumber)
+        lastNumber = thisNumber
+        thisNumber = nextNumber
+
+    print(listNumber)
+
+if __name__ == '__main__':
+    fibonnacci()
